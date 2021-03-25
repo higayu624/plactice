5a14080c8f003d87e8d416d0329adc2ff7f54f94
higayu624 <higayu624@gmail.com>
2021-03-25 18:49:40 +0900

    hey

---
+++ index.php
@@ -0,0 +1,13 @@
+<!DOCTYPE html>
+<html lang="en">
+    <head>
+        <meta charset="UTF-8">
+        <title>Golang Template Test</title>
+    </head>
+    <body>
+        <h1>{{.}}</h1>
+        <h2>{{.Name}}</h2>
+        <h2>{{.Age}}</h2>
+        <h1>hello!</h1>
+    </body>
+</html>
